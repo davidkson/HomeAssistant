f7bdd8ad08239cfa0134b7d8233d6f853ccb586f
davidkson <davidkson@gmail.com>
2022-05-20 13:11:22 +0200

    Update wled_gradient.py

--- wled_gradient.py
+++ wled_gradient.py
@@ -1,15 +1,32 @@
-rgb = []
-r = 0
-g = 0
-b = 0
-reverse = False
-gradient_grade = 0.5
-
 start_led = data.get("start_led")
 stop_led = data.get("stop_led")
 active_led_count = data.get("active_led_count")
 style = data.get("style")
 ip_address = data.get("ip_address")
+
+def red_to_green(r, g, b):
+    if(g < 255):
+        g = min(255, g + increment)
+    elif(r > 0):
+        r = max(0, r - decrement)
+    return (r,g,b)
+
+def green_to_red(r, g, b):
+    if(r < 255):
+        r = min(255, r + increment)
+    elif(g > 0):
+        g = max(0, g - decrement)
+    return (r,g,b)
+
+def toRgbString(t):
+    return "[" + str(t[0]) + ", " + str(t[1]) + ", " + str(t[2]) + "]"
+
+reverse = False
+r = 0
+g = 0
+b = 0
+gradientFunc = lambda r,g,b : (r,g,b)
+gradient_grade = 0.5
 
 if(stop_led < start_led):
     reverse = True
@@ -21,10 +38,12 @@ if(style == 'red_green'):
     r = 255
     g = 0
     gradient_grade = 0.75
+    gradientFunc = red_to_green
 elif(style == 'green_red'):
     r = 0
     g = 255
     gradient_grade = 0.25
+    gradientFunc = green_to_red
 
 total_led_count = (stop_led - start_led) + 1
 increment_count = round(total_led_count * gradient_grade)
@@ -33,30 +52,25 @@ decrement_count = total_led_count - increment_count
 increment = round(255/increment_count)
 decrement = round(255/decrement_count)
 
-for i in range(0,total_led_count):
-    if i < active_led_count:
-        rgb.append("[" + str(r)  + "," + str(g) + "," + str(b) + "]")
-        if(style == 'red_green'):
-            if(g < 255):
-                g = min(255,g + increment)
-            elif(r > 0):
-                r = max(0,r - decrement)
-        elif(style == 'green_red'):
-            if(r < 255):
-                r = min(255, r + increment)
-            elif(g > 0):
-                g = max(0, g - decrement)
-    else:
-        rgb.append("[0,0,0]")        
+rgb = []
 
-output = ""  
-for index, x in enumerate(rgb):
+for current_led in range(0, total_led_count):
+    if current_led < active_led_count:        
+        rgb.append((r,g,b))
+        r,g,b = gradientFunc(r, g, b)        
+    else:        
+        rgb.append((0,0,0))
+
+payload = ""  
+for index, rgb_value in enumerate(rgb):
     if reverse:
-        output = output + str((stop_led - index-1)) + "," + x + ","
+        ledIndex = (stop_led - index - 1)                
     else:
-        output = output +  str((index + start_led-1)) + "," + x + ","
-    
-output = '{"seg":{"i":[' + output[:-1] + ']}}'
+        ledIndex = (index + start_led-1)
 
-service_data = {"url" : "http://" + ip_address + "/json/state", "payload" : output }
+    payload = payload + str(ledIndex) + "," + toRgbString(rgb_value) + ","
+    
+payload = '{"seg":{"i":[' + payload[:-1] + ']}}'
+
+service_data = {"url" : "http://" + ip_address + "/json/state", "payload" : payload }
 hass.services.call('rest_command','post', service_data, False)
